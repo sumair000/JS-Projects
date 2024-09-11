9397c40620b633803e5e910f42afb7b3afe1f3af
sumair <sumairkhan0091@gmail.com>
2024-09-11 17:05:27 +0500

    now switch function is working correctly

--- index.js
+++ index.js
@@ -28,22 +28,65 @@ function start() {
 
   document.getElementById("startButton").disabled = true;
 
-  turnMessage(toss)
+  const currentPlayer = player.find( p => p.turn === true);
+
+  turnMessage(currentPlayer.id);
 
 
 }
 
-function turnMessage(toss){
+function turnMessage(id){
 
-  // console.log("toss value is: ",toss);
-
-  document.getElementById(`player${toss + 1}message`).innerHTML = `${player[toss].name}`;
-
+  document.getElementById(`player${id}message`).innerHTML = `${player[id-1].name}`;
+  
 }
+
+document.getElementById("rollDice").addEventListener("click", diceRoll);
+
 
 function diceRoll(){
 
+  let roll = Math.floor(Math.random() * 6) + 1;
+
+  const currentPlayer = player.find( p=> p.turn === true);
+   
+  changeDiceImages(roll, currentPlayer.id);
+
+  countScore(roll);
+
+  if (roll === 1) {
+    switchPlayer();
+  }
+
+
+
 }
 
+function changeDiceImages(roll, playerId){
 
+  document.getElementById(`dice-img${playerId}`).src = `images/${roll}.svg`;
+
+}
+
+function countScore(roll){
+
+  const currentPlayer = player.find( p=> p.turn === true);
+
+  currentPlayer.score += roll;
+
+  console.log(currentPlayer.score);
+  
+   
+}
+
+function switchPlayer(roll){
+
+    const currentPlayer = player.find( p=> p.turn === true)
+
+    const nextPlayer = player.find( p=> p.turn === false)
+
+    nextPlayer.turn = true;
+    currentPlayer.turn = false;
+   
+}
 
