5d80b3f973eadee82eef45d38b9d6303fe004653
sumair <sumairkhan0091@gmail.com>
2024-09-11 18:30:47 +0500

    abhi thora kam baki hy

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ function turnMessage(id){
 
 document.getElementById("rollDice").addEventListener("click", diceRoll);
 
+let count = 0;
 
 function diceRoll(){
 
@@ -55,10 +56,24 @@ function diceRoll(){
   countScore(roll);
 
   if (roll === 1) {
+    count += 1;
+    if (count === 2) {
+
+      document.getElementById('rollDice').disabled = true;
+      
+      player[0].turn = true;
+      player[1].turn = true;
+      
+      winner();
+      return;
+      
+    }
+    
     switchPlayer();
+    
   }
 
-
+  
 
 }
 
@@ -73,13 +88,10 @@ function countScore(roll){
   const currentPlayer = player.find( p=> p.turn === true);
 
   currentPlayer.score += roll;
-
-  console.log(currentPlayer.score);
-  
-   
+    
 }
 
-function switchPlayer(roll){
+function switchPlayer(){
 
     const currentPlayer = player.find( p=> p.turn === true)
 
@@ -87,6 +99,40 @@ function switchPlayer(roll){
 
     nextPlayer.turn = true;
     currentPlayer.turn = false;
+
+    turnMessage(nextPlayer.id);
    
 }
+
+function winner(){
+
+  const player1 = player[0];
+  const player2 = player[1];
+
+
+    if ((player1.turn && player2.turn)) {
+
+        if (player1.score > player2.score) {
+
+          showResult(player1.id);
+
+        } else if (player1.score < player2.score) {
+
+          showResult(player2.id);
+        } else {
+
+          alert("MATCH DRAW...");
+
+        }
+      
+    }
+
+}
+
+function showResult(id){
+
+  document.getElementById(`p${id}-won`).innerHTML = `Player ${id} WON...😎`;
+
+}
+
 
