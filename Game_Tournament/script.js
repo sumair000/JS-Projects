484554176ade3ea79f373faa1f3321cf425e996b
sumair <sumairkhan0091@gmail.com>
2024-09-12 16:14:33 +0500

    declear array , having 8 object , players data

---
+++ Game_Tournament/script.js
@@ -0,0 +1,59 @@
+
+const groupMatches = [
+  {
+    id: 1,
+    name: "Player 1",
+    score: 0,
+    turn: false,
+  },
+
+  {
+    id: 2,
+    name: "Player 2",
+    score: 0,
+    turn: false,
+  },
+
+  {
+    id: 3,
+    name: "Player 3",
+    score: 0,
+    turn: false,
+  },
+
+  {
+    id: 4,
+    name: "Player 4",
+    score: 0,
+    turn: false,
+  },
+
+  {
+    id: 5,
+    name: "Player 5",
+    score: 0,
+    turn: false,
+  },
+
+  {
+    id: 6,
+    name: "Player 6",
+    score: 0,
+    turn: false,
+  },
+
+  {
+    id: 7,
+    name: "Player 7",
+    score: 0,
+    turn: false,
+  },
+
+  {
+    id: 8,
+    name: "Player 8",
+    score: 0,
+    turn: false,
+  },
+  
+];
