ed4eccd9c4ff9f0ad3efc4e669e0cb5beb206804
sumair <sumairkhan0091@gmail.com>
2024-09-13 17:00:47 +0500

    QuaterFinal matches are done, everything is good

--- Game_Tournament/script.js
+++ Game_Tournament/script.js
@@ -1,4 +1,5 @@
 
+
 const player = [
   {
     id: 1,
@@ -55,87 +56,105 @@ const player = [
     score: 0,
     turn: false,
   },
-
 ];
 
-document.getElementById('startButton').addEventListener('click',startGame);
+let first = 1;
+let second = 5;
 
-document.getElementById('tossButton').addEventListener('click',toss);
+let playMatch;
+
+document.getElementById("startButton").addEventListener("click", startGame);
+
+document.getElementById("tossButton").addEventListener("click", toss);
 
 document.getElementById("rollDice").addEventListener("click", diceRoll);
 
+document.getElementById('nextMatch').addEventListener('click',reset);
 
 let matchNumber = 0;
 
-function startGame(){
+function startGame() {
 
-    document.getElementById("startButton").disabled = true;
-    matchNumber += 1;
-    showMessage(matchNumber);
-}
-
-function showMessage(number){
-
-    alert(`Mathch No: ${number} started!...`)
-}
-
-let playMatch = player.filter(pair => {
-    
-    return pair.id === 1 || pair.id === 5
-    })
-    playMatch[0].id = 1;
-    playMatch[1].id = 2;
-
-    console.log(playMatch);
-
-function toss(){
-
-    document.getElementById("tossButton").disabled = true;
-    
-    let wonToss = Math.random() < 0.5 ? 0: 1;
-
-    // console.log(playMatch);
-    
-
-    playMatch[wonToss].turn = true;
-
-    console.log(playMatch[wonToss].name);
-    // console.log(playMatch[wonToss].id);
-    
-    
-    turnMessage(wonToss);
-
-}
-
-
-function turnMessage(id){
-
-
-    console.log('array idex',id);
-
-    console.log('--------------------------->');
-
-    
-  const message = document.createElement('h3');
-
-  message.innerText = `${playMatch[id].name}'s turn`;
-
-  document.getElementById('playermessage').appendChild(message);
+  document.getElementById("startButton").disabled = true;
+  matchNumber += 1;
+  showMessage(matchNumber);
 
   
+  updateIds(first,second);
+  first +=1;
+  second +=1;
+    console.log(first,second);
+    
+}
+
+function showMessage(number) {
+
+  alert(`Mathch No: ${number} started!...`);
+
+}
+
+function updateIds(newId1, newId2) {
+
+    id1 = newId1;
+    id2 = newId2;
+
+    playMatch = player.filter((pair) => {
+
+      return pair.id === id1 || pair.id === id2;
+
+    });
+
+    playMatch[0].id = 1;
+    playMatch[1].id = 2;
+    console.log(playMatch); 
+
+    return playMatch;
+}
+
+
+function toss() {
+
+  document.getElementById("tossButton").disabled = true;
+
+  let wonToss = Math.random() < 0.5 ? 0 : 1;
+
+  // console.log(playMatch);
+
+  playMatch[wonToss].turn = true;
+
+  console.log(playMatch[wonToss].name);
+  // console.log(playMatch[wonToss].id);
+
+  turnMessage(wonToss);
+
+}
+
+function turnMessage(id) {
+
+  console.log("array idex", id);
+
+  console.log("--------------------------->");
+
+  const message = document.createElement("h3");
+
+  message.id = "turnMessage";
+  message.innerText = `${playMatch[id].name}'s turn`;
+
+  document.getElementById("playermessage").appendChild(message);
+
+
 }
 
 let count = 0;
 
-function diceRoll(){
+function diceRoll() {
 
   let roll = Math.floor(Math.random() * 6) + 1;
 
-  const currentPlayer = playMatch.find( p=> p.turn === true);
+  const currentPlayer = playMatch.find((p) => p.turn === true);
 
-  console.log('dice before id',currentPlayer.id);
+  console.log("dice before id", currentPlayer.id);
 
-  
   changeDiceImages(roll, currentPlayer.id);
 
   countScore(roll);
@@ -143,116 +162,116 @@ function diceRoll(){
   if (roll === 1) {
     count += 1;
     if (count === 2) {
-      
-        playMatch[0].turn = true;
-        playMatch[1].turn = true;
-      
+      playMatch[0].turn = true;
+      playMatch[1].turn = true;
+
       winner();
       return;
-      
     }
-    
-   switchPlayer();
- 
+
+    switchPlayer();
   }
   winner();
 
 }
 
+function changeDiceImages(roll, playerId) {
 
-function changeDiceImages(roll, playerId){
+  console.log("ch img id: ", playerId);
 
-    console.log('ch img id: ',playerId);
-    
-
-    document.getElementById(`dice-img${playerId}`).src = `images/${roll}.svg`;
-  
-  }
-
-function countScore(roll){
-
-    const currentPlayer = playMatch.find( p=> p.turn === true);
-  
-    currentPlayer.score += roll;
-
-    document.getElementById(`score-area${currentPlayer.id}`).innerHTML = `score: ${currentPlayer.score}`;
-
-    console.log('score--->',currentPlayer.score);
-    
+  document.getElementById(`dice-img${playerId}`).src = `images/${roll}.svg`;
 
 }
 
-function switchPlayer(){
+function countScore(roll) {
 
-    const currentPlayer = playMatch.find( p=> p.turn === true)
+  const currentPlayer = playMatch.find((p) => p.turn === true);
 
-    const nextPlayer = playMatch.find( p=> p.turn === false)
+  currentPlayer.score += roll;
 
-    nextPlayer.turn = true;
-    currentPlayer.turn = false;
+  document.getElementById(`score-area${currentPlayer.id}`).innerHTML = `score: ${currentPlayer.score}`;
 
-    console.log('next player id',nextPlayer.id-1);
-    
-    document.getElementById('playermessage').innerHTML = "";
-    turnMessage(nextPlayer.id-1);
-   
+  console.log("score--->", currentPlayer.score);
+
 }
 
+function switchPlayer() {
 
-function winner(){
+  const currentPlayer = playMatch.find((p) => p.turn === true);
 
-    const player1 = playMatch[0];
-    const player2 = playMatch[1];
-  
-    if ((player1.turn && player2.turn)) {
-  
-      if (player1.score > player2.score) {
-  
-        showResult(player1.id);
-        return;
-  
-      } else if (player1.score < player2.score) {
-  
-        showResult(player2.id);
-        return;
-      } else {
-  
-        alert("MATCH DRAW...");
-        return;
-  
-      }
-    
-  }
-  
-    const nextPlayer = playMatch.find( p=> p.turn === false)
-  
-    const currentPlayer = playMatch.find( p=> p.turn === true);
-  
-    if (nextPlayer.turn === false && nextPlayer.score !== 0) {
-      
-      if (currentPlayer.score > nextPlayer.score) {
-        
-        showResult(currentPlayer.id);
-  
-  
-        return;
-  
-      }
-  
+  const nextPlayer = playMatch.find((p) => p.turn === false);
+
+  nextPlayer.turn = true;
+
+  currentPlayer.turn = false;
+
+  console.log("next player id", nextPlayer.id - 1);
+
+  document.getElementById("playermessage").innerHTML = "";
+
+  turnMessage(nextPlayer.id - 1);
+
+}
+
+function winner() {
+
+  const player1 = playMatch[0];
+  const player2 = playMatch[1];
+
+  if (player1.turn && player2.turn) {
+    if (player1.score > player2.score) {
+      showResult(player1.id);
+      return;
+    } else if (player1.score < player2.score) {
+      showResult(player2.id);
+      return;
+    } else {
+      alert("MATCH DRAW...");
+        document.getElementById("rollDice").disabled = true;
+      return;
     }
-    
-  
-  }
-  
-  function showResult(id){
-  
-    document.getElementById(`p-won`).innerHTML = `Player ${id} WON...😎`;
-  
-    document.getElementById('rollDice').disabled = true;
-  
-  
   }
 
-  
+  const nextPlayer = playMatch.find((p) => p.turn === false);
 
-  
+  const currentPlayer = playMatch.find((p) => p.turn === true);
+
+  if (nextPlayer.turn === false && nextPlayer.score !== 0) {
+    if (currentPlayer.score > nextPlayer.score) {
+      showResult(currentPlayer.name);
+
+      return;
+    }
+  }
+
+}
+
+function showResult(id) {
+
+  document.getElementById(`p-won`).innerHTML = `Player ${id} WON...😎`;
+
+  document.getElementById("rollDice").disabled = true;
+
+
+}
+
+function reset(){
+
+    playMatch[0].id = 0;
+    playMatch[1].id = 0;
+
+    document.getElementById("turnMessage").innerHTML = "";
+    document.getElementById(`score-area1`).innerHTML = "";
+    document.getElementById(`score-area2`).innerHTML = "";
+    document.getElementById(`p-won`).innerHTML = "";
+
+
+  document.getElementById("startButton").disabled = false;
+
+  document.getElementById("tossButton").disabled = false;
+
+  document.getElementById("rollDice").disabled = false;
+
+
+}
+
